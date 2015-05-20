3abeacb1bfa3d0c8a5f70c0b6c7e95c486809d95
Ivan Vashchenko <srochno@gmail.com>
2015-05-20 07:13:04 -0400

    Add token functionality

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 var https = require("https");
 var Geoservices = require("geoservices");
 var edump = require("esri-dump");
-
-var url = "http://route.arcgis.com/arcgis/rest/services/World/
-Route/NAServer/Route_World/solve?token=#{esri_token}&stops=
-#{endpoints.join(';')}&f=json";
+var request = require("request");
 
 var geoCodeFromAndTo = function(from, to, callback) {
 	var client = new Geoservices(),
@@ -32,8 +29,35 @@ var geoCodeFromAndTo = function(from, to, callback) {
 
 };
 
-var getDrivingTime = function(from, to, callback) {
-	var geoJson = edump()
+var getDrivingTime = function(eId, eSecret, from, to, callback) {
+	var token,
+		restUrl = "http://route.arcgis.com/arcgis/rest/",
+		routePath = "services/World/Route/NAServer/Route_World/";
+	
+	request.post(
+		'https://www.arcgis.com/sharing/rest/oauth2/token',
+		{form: {
+			f: 'json',
+			client_id: eId,
+			client_secret: eSecret,
+			grant_type: 'client_credentials'
+		}},
+		function(error, res, body) {
+			if (error) {
+				console.log(error);
+			} else if (res.statusCode == 200) {
+				token = JSON.parse(body).access_token;
+				console.log(token);
+
+				
+				var routeParams = "solve?token=" + token + 
+					"&stops=" +  + "&f=json";
+				//var geoJson = edump(restUrl + routePath + routeParams);
+			}
+		}
+	);
+
+
 };
 
 module.exports.getDrivingTime = getDrivingTime;
--- test.js
+++ test.js
@@ -1,6 +1,10 @@
 var dtime = require("./index.js");
-//269 Varick St, Jersey City NJ 07302
-dtime.getDrivingTime("neasdfsdf", 
-	"63 New Hook Rd, Bayonne NJ 07002", function(from, to) {
-		console.log(from, to);
+
+var esriCliendId = 'o7U4CF9NDgDtVryb',
+	esriClientSecret = 'f331ecdd0fe74a7a996ec514319578dc';
+
+dtime.getDrivingTime(esriCliendId, esriClientSecret,
+	"269 Varick St, Jersey City NJ 07302", 
+	"63 New Hook Rd, Bayonne NJ 07002", function(result) {
+		console.log(result);
 	});
